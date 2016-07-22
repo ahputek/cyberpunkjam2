d82633dd366c61810ae9ab70a0b91871792d27b0
Chryse <chrysedev@gmail.com>
2016-07-22 22:17:03 +0800

    Add FSM for turn-based system

--- CyberpunkJam2/Assets/Scripts/Fight/FightSystem.cs
+++ CyberpunkJam2/Assets/Scripts/Fight/FightSystem.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class FightSystem : MonoBehaviour {
 
-	private enum Turn { RobotA, RobotB };
+	private enum Turn { IDLE, ROBOT_A, ROBOT_B, DONE };
 
 	[SerializeField]
 	private Turn turn;
@@ -11,58 +11,74 @@ public class FightSystem : MonoBehaviour {
 	private RobotModel robotA;
 	private RobotModel robotB;
 
-	private void Start () {
-		StartCoroutine(TakeTurn());
+	private void Awake () {
+		this.turn = Turn.IDLE;
+
+		StartCoroutine(FSM());
 	}
 
-	private IEnumerator TakeTurn () {
-		bool flag = true;
-		while(flag) {
-			CyberpunkApplication app = CyberpunkApplication.Instance;
-			if(app.Model.Fight != null) {
-				this.robotA = app.Model.Fight.Robots[0];
-				this.robotB = app.Model.Fight.Robots[1];
-			}
-
-			yield return new WaitForSeconds(2);
-			if(robotA != null && robotB != null) {
-				Attack(robotA, robotB);
-			}
-
-//			yield return new WaitForSeconds(0.5f);
-//			if(robotA != null && robotB != null) {
-//				ResolveRobotStatus(robotA, robotB);
-//				flag = IsAlive(robotB);
-//			}
-
-			yield return new WaitForSeconds(2);
-			if(robotA != null && robotB != null) {
-				Attack(robotB, robotA);
-			}
-
-//			yield return new WaitForSeconds(0.5f);
-//			if(robotA != null && robotB != null) {
-//				ResolveRobotStatus(robotB, robotA);
-//				flag = IsAlive(robotA);
-//			}
+	// FSM will be called every frame
+	private IEnumerator FSM () {
+		while(true) {
+			yield return StartCoroutine(this.turn.ToString());
 		}
+	}
+
+	private IEnumerator IDLE () {
+		yield return null;
+
+		yield return new WaitUntil(() => this.turn != Turn.IDLE);
+	}
+
+	private IEnumerator ROBOT_A () {
+		yield return null;
+		CyberpunkApplication app = CyberpunkApplication.Instance;
+		if(app.Model.Fight != null) {
+			this.robotA = app.Model.Fight.Robots[0];
+			this.robotB = app.Model.Fight.Robots[1];
+		}
+
+		Attack(robotA, robotB);
+		yield return new WaitUntil(() => this.turn != Turn.ROBOT_A);
+	}
+
+	private IEnumerator ROBOT_B () {
+		yield return null;
+		CyberpunkApplication app = CyberpunkApplication.Instance;
+		if(app.Model.Fight != null) {
+			this.robotA = app.Model.Fight.Robots[0];
+			this.robotB = app.Model.Fight.Robots[1];
+		}
+
+		Attack(robotB, robotA);
+		yield return new WaitUntil(() => this.turn != Turn.ROBOT_B);
+	}
+
+	private IEnumerator DONE () {
+		yield return null;
+
+		yield return new WaitUntil(() => this.turn != Turn.DONE);
 	}
 
 	private void Attack (RobotModel attacker, RobotModel target) {
 		CyberpunkApplication app = CyberpunkApplication.Instance;
 		app.Notify(Constants.ATTACK, app.Controller.Robot, attacker, target);
 		app.Notify(Constants.UPDATE_FIGHT, app.Controller.Fight,  app.Model.Fight);
+
+		ResolveRobotStatus(attacker, target);
 	}
 
 	private void ResolveRobotStatus (RobotModel attacker, RobotModel target) {
-		if(IsAlive(target)) {
+		if(!IsAlive(target)) {
 			CyberpunkApplication app = CyberpunkApplication.Instance;
-			app.Notify(Constants.WIN, app.Controller.Robot, target);
-			app.Notify(Constants.LOSE, app.Controller.Robot, attacker);
+			app.Notify(Constants.WIN, app.Controller.Robot, attacker);
+			app.Notify(Constants.LOSE, app.Controller.Robot, target);
 		}
 	}
 
 	private bool IsAlive (RobotModel robot) {
 		return robot.Health > 0;
 	}
+
+
 }
